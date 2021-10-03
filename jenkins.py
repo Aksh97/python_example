b0ce2de83e274f047fbe49f1a19072be095ad188
Akshay Sachdeva <36452065+Aksh97@users.noreply.github.com>
2021-10-03 19:05:44 +0800

    Add files via upload

---
+++ jenkins.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  3 18:24:19 2021
+
+@author: Works User
+"""
+
+import os
+
+List1 = [8,9,3,6,1,10]
+List1.reverse()
+print("Reversed List is ", List1)
+
+List2 = [91,67,120, 34,76,54,78,87,56,64,354]
+List2.sort()
+print("Sorted List is ", List2)
+
+List3=[]
+List3=List1.copy()
+print("List 3 ", List3)
+
+indexvalue = List2[2:6]
+print("The index value are ", indexvalue)
+
